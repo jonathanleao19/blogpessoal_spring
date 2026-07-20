d366a206bbe36076137a98605b59cd56421bdfa5
Jonathan Leao <jonathan.sleao@gmail.com>
2026-07-20 20:13:04 -0300

    ✨ Finalização do CRUD - Recurso Postagem

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -19,6 +19,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -29,6 +30,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -54,23 +58,36 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(postagemRepository.save(postagem));
-		
-		// INSERT INTO tb_postagens(titulo, texto) VALUES(?, ?);
-		
-	}
+		if (temaRepository.existsById(postagem.getTema().getId())) {
+
+            postagem.setId(null);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(postagemRepository.save(postagem));
+
+            // INSERT INTO tb_postagens(titulo, texto) VALUES(?, ?);
+        }
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O tema não existe!", null);
+
+     }
 	
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-		
-		if(postagemRepository.existsById(postagem.getId()))
-		return ResponseEntity.ok(postagemRepository.save(postagem));
-		
-		// UPDATE tb_postagens SET titulo = ?, texto = ? WHERE id = ?);
-		
-		return ResponseEntity.notFound().build();
-	}
+		if(postagemRepository.existsById(postagem.getId())) {
+
+            if (temaRepository.existsById(postagem.getTema().getId())) { 
+                return ResponseEntity.ok(postagemRepository.save(postagem));
+                
+            // UPDATE tb_postagens SET titulo = ?, texto = ? WHERE id = ?;
+     }
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O tema não existe!", null);
+
+     }
+
+        return ResponseEntity.notFound().build();
+     }
 	
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id) {
